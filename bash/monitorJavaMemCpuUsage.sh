52364b51de7120a6472d6c7911cf5ae6139b40f9
Mathieu BERTHAULT <83458460+mberthault@users.noreply.github.com>
2025-06-21 16:00:19 +0200

    Update monitorJavaMemCpuUsage.sh

--- bash/monitorJavaMemCpuUsage.sh
+++ bash/monitorJavaMemCpuUsage.sh
@@ -5,3 +5,13 @@ touch monitorJavaMemCpuUsage.log
 # ps aux --sort=+pid | awk 'NR==1 || /[j]ava/' >> monitorJavaMemCpuUsage.log
 echo "-----------------------------------------------" >> monitorJavaMemCpuUsage.log
 ps -eo pid,%mem,%cpu,command --sort=pid | awk 'NR==1 || /[j]ava/' | sed "s#^#$(date +%Y"-"%m"-"%d"_"%H"."%M"."%S) #" >> monitorJavaMemCpuUsage.log
+
+## Use pidstat ??
+#e.g. to monitor these two process IDs (12345 and 11223) every 5 seconds use
+#$ pidstat -h -r -u -v -p 12345,11223 5
+
+## Use ttop ??
+# https://serverfault.com/questions/387268/linux-cpu-usage-and-process-execution-history
+
+##
+## Check : https://linuxconfig.org/ps-output-difference-between-vsz-vs-rss-memory-usage
