ccacd69f7f5b38e7e2dfdb916fca6a7408bba1a1
Mathieu BERTHAULT <83458460+mberthault@users.noreply.github.com>
2025-06-21 15:46:15 +0200

    Create monitorJavaMemCpuUsage.sh

---
+++ bash/monitorJavaMemCpuUsage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -f monitorJavaMemCpuUsage.log
+touch monitorJavaMemCpuUsage.log
+
+# ps aux --sort=+pid | awk 'NR==1 || /[j]ava/' >> monitorJavaMemCpuUsage.log
+echo "-----------------------------------------------" >> monitorJavaMemCpuUsage.log
+date +%Y"-"%m"-"%d"_"%H"."%M"."%S >> monitorJavaMemCpuUsage.log
+ps -eo pid,%mem,%cpu,command --sort=pid | awk 'NR==1 || /[j]ava/' >> monitorJavaMemCpuUsage.log
